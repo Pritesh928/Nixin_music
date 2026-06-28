497fcfe3ca5eb17c416fe85015b9c899f104a71a
Pritesh <pptsingh123@gmail.com>
2026-06-28 13:09:41 +0530

    chore : Retrofitclient init

--- app/src/main/java/com/firstapp/nixin_music/ApiService.kt
+++ app/src/main/java/com/firstapp/nixin_music/ApiService.kt
@@ -1,4 +1,10 @@
 package com.firstapp.nixin_music
 
-class ApiService {
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+    @GET("api/search")
+    fun search(@Query("q") query: String): Call<List<VideoItem>>
 }
